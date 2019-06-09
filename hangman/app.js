f73614d058aded490bc8913e3ea7f72e07a63369
jinook929 <jinook929@gmail.com>
2019-06-08 21:11:16 -0400

    finish hangman game

--- hangman/app.js
+++ hangman/app.js
@@ -1,63 +1,14 @@
-// Primitive Value: string, number, boolean, null, undefined
+const game1 = new Hangman('Cat', 2);
+game1.init();
+// const game2 = new Hangman('Running Zebra', 3);
+// game2.init();
 
-// Object: myObject --> Object.prototype --> null
-// Array: myArray --> Array.prototype --> Object.prototype --> null
-// Function: myFunction --> Function.prototype --> Object.prototype --> null
-// String: myString --> String.prototype --> Object.prototype --> null
-// Number: myNumber --> Number.prototype --> Object.prototype --> null
-// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
-
-const product = {
-  name: 'Influence'
-};
-
-Object.prototype.someNewMethod = () => 'This is a new function'
-console.log('name@product', product.hasOwnProperty('name'))
-console.log('price@product', product.hasOwnProperty('price'))
-console.log('someNewMethod@product', product.hasOwnProperty('someNewMethod'))
-console.log('someNewMethod@product.__proto__', product.__proto__.hasOwnProperty('someNewMethod'))
-console.log('someNewMethod', product.someNewMethod())
-console.log(product)
-
-const product1 = new Object();
-console.log(product1)
-product1.name = 'rest'
-console.log(product1)
-
-const product2 = new Object({
-  name: 'The War of Art'
+window.addEventListener('keypress', (e) => {
+  let guess = e.key;
+  if(guess === 'Enter' || guess === ' ' || game1.guessedLetters.includes(guess)) {
+  // if(guess === 'Enter' || guess === ' ' || game2.guessedLetters.includes(guess)) {
+    return;
+  }
+  game1.makeGuess(guess)
+  // game2.makeGuess(guess);
 });
-console.log(product2)
-
-const team = ['Tim', 'Maddy'];
-console.log(team)
-console.log('length method@team',team.hasOwnProperty('length'))
-console.log('filter method@team',team.hasOwnProperty('filter'))
-console.log('filter method@prototype',team.__proto__.hasOwnProperty('filter'))
-console.log(team.someNewMethod())
-
-const team1 = new Array();
-console.log(team1)
-team1[0] = 'rest'
-console.log(team1)
-
-const team2 = new Array('Tom', 'Mulan');
-console.log(team2)
-
-const getScore = () => 1;
-console.log(getScore)
-
-const functionExample = function() {
-  return 3;
-}
-console.log(functionExample.__proto__.hasOwnProperty('toString'))
-console.log(functionExample.__proto__.__proto__.hasOwnProperty('hasOwnProperty'))
-
-const item = 'Computer'
-console.log(item)
-
-const otherItem = new String('Phone');
-console.log(otherItem)
-
-const num = new Number(123);
-console.log(num)
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,14 +1,15 @@
-const Hangman = function(word, remainingGuesses) {
+const Hangman = function (word, remainingGuesses) {
   this.word = word.toLowerCase().split('');
   this.remainingGuesses = remainingGuesses;
   this.guessedLetters = [];
+  this.status = 'Playing';
 }
 
-Hangman.prototype.getPuzzle = function() {
+Hangman.prototype.getPuzzle = function () {
   let puzzle = '';
-  
+
   this.word.forEach(letter => {
-    if(this.guessedLetters.includes(letter) || letter === ' ') {
+    if (this.guessedLetters.includes(letter) || letter === ' ') {
       puzzle += letter;
     } else {
       puzzle += '*';
@@ -18,32 +19,85 @@ Hangman.prototype.getPuzzle = function() {
   return puzzle;
 }
 
-Hangman.prototype.makeGuess = function(guess) {
+Hangman.prototype.makeGuess = function (guess) {
   guess = guess.toLowerCase();
   let isUnique = !this.guessedLetters.includes(guess);
   let isWrong = !this.word.includes(guess.toLowerCase());
+  
+  if(this.status !== 'Playing') { // failed or finished situation
+    return;
+  } else { // playing situation
+    if (isUnique && isWrong) { // decrement remaining guesses
+      this.remainingGuesses--;
+    }
 
-  if(guess && isUnique && isWrong) {
-    this.remainingGuesses--;
-  }
+    if (isUnique) { // add guess letter to guessedLetters array
+      this.guessedLetters.push(guess);
+    }
 
-  if(guess && isUnique) {
-    this.guessedLetters.push(guess);
+    // display gameMsg
+    gameMsg.innerHTML = `Guess #${this.guessedLetters.length} - "${guess}" tried: ${this.getPuzzle()} [${this.remainingGuesses} guess(es) left]<p><small>(Letters You Tried: ${this.guessedLetters.sort().join()})</small></p>`;
+
+    this.calculatingStatus(); // calculate playing status, then print game result
+    this.statusMsg(); // display message based on status
   }
 }
 
-const game1 = new Hangman('Cat', 2); // console.log(game1.getPuzzle(), game1.remainingGuesses)
-
-const game2 = new Hangman('Running Zebra', 3); console.log(game2.getPuzzle(), game2.remainingGuesses)
-
-window.addEventListener('keypress', (e) => {
-  // let guess = String.fromCharCode(e.charCode); --> Andrew's way
-  let guess = e.key;
-  if(guess === 'Enter' || guess === ' ' || game2.guessedLetters.includes(guess)) {
-    return;
+Hangman.prototype.statusMsg = function () {
+  let gameResult = document.createElement('h4')
+  if(this.status === 'Failed') {
+    console.log('failed')
+    gameResult.innerHTML = `Nice try! The answer was "${this.word.join('')}".`;
+    puzzleDisplay.appendChild(gameResult);
+  } else if(this.status === 'Finished') {
+    console.log('finished')
+    gameResult.innerHTML = `Great work! You guessed the word.`;
+    puzzleDisplay.appendChild(gameResult);
+  } else {
+    console.log('playing')
   }
-  // game1.makeGuess(guess)
-  game2.makeGuess(guess)
-  // console.log(`Game1 - "${guess}" tried: ${game1.getPuzzle()} [${game1.remainingGuesses} chance(s) left]`)
-  console.log(`Game2 - "${guess}" tried: ${game2.getPuzzle()} [${game2.remainingGuesses} chance(s) left]`)
-})
+}
+
+Hangman.prototype.calculatingStatus = function () {
+  let answerletters = this.word.filter(e => String(e).trim());
+  // Way #1. sort
+  // answerletters = [...new Set(answerletters)].sort();
+  // function array_diff(a, b) {
+  //   var tmp={}, res=[];
+  //   for(var i=0;i<a.length;i++) tmp[a[i]]=1;
+  //   for(var i=0;i<b.length;i++) { if(tmp[b[i]]) delete tmp[b[i]]; }
+  //   for(var k in tmp) res.push(k);
+  //   return res;
+  // }
+
+  // Way #2. forEach
+  // answerletters.forEach((letter) => {
+  //   if(this.guessedLetters.includes(letter)) {
+
+  //   } else {
+  //     finished = false;
+  //   }
+  // });
+  // console.log(finished)
+
+  // Way #3. filter
+  // let remainingLetters = answerletters.filter(letter => {
+  //   return !this.guessedLetters.includes(letter);
+  // });
+
+  // Way #4. every
+  let finished = answerletters.every(letter => this.guessedLetters.includes(letter)); 
+  if (this.remainingGuesses === 0) {
+    this.status = 'Failed';
+  } else if (finished) {
+    this.status = 'Finished';
+  } else {
+    this.status = 'Playing';
+  }
+}
+
+Hangman.prototype.init = function () {
+  const puzzleDisplay = document.querySelector('#puzzleDisplay');
+  puzzleDisplay.innerHTML = '';
+  puzzleDisplay.innerHTML = `<h3>Game Word: ${this.getPuzzle()} [You have ${this.remainingGuesses} guessing chances]</h3><p id="gameMsg">Make your guess and just type it.</p>`;
+}
