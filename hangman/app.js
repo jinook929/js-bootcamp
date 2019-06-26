de88a6a3eb5dc7d93b38772ecd2c5347e7a051bc
jinook929 <jinook929@gmail.com>
2019-06-25 22:18:19 -0400

    review promise conversion

--- hangman/app.js
+++ hangman/app.js
@@ -1,5 +1,22 @@
-const game = new Hangman('newPuzzle', 3);
-game.init();
+const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener('readystatechange', (e) => {
+    if(e.target.readyState === 4 && e.target.status === 200) {
+      const data = JSON.parse(e.target.responseText);
+      resolve(data.puzzle)
+    } else if(e.target.readyState === 4) {
+      reject('An error has taken place')
+    }
+  })
+
+  request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+  request.send();
+})
+
+// const game = new Hangman('Troy', 3);
+
+// game.init();
 
 window.addEventListener('keypress', (e) => {
   let guess = String.fromCharCode(e.charCode); // guess = e.key;
@@ -9,9 +26,10 @@ window.addEventListener('keypress', (e) => {
   game.makeGuess(guess)
 });
 
-let newPuzzle = getPuzzle('2').then((puzzle) => {
-  console.log(puzzle)
+let game = {};
+getPuzzle('2').then((puzzle) => {
+  game = new Hangman(puzzle, 5); 
+  game.init(); console.log('Random Word: ',puzzle); 
 }, (err) => {
   console.log(`Error: ${err}`)
 });
-console.log(newPuzzle)
--- hangman/requests.js
+++ hangman/requests.js
@@ -1,18 +1,18 @@
-const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
-  const request = new XMLHttpRequest();
+// const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
+//   const request = new XMLHttpRequest();
 
-  request.addEventListener('readystatechange', (e) => {
-    if(e.target.readyState === 4 && e.target.status === 200) {
-      const data = JSON.parse(e.target.responseText);
-      resolve(data.puzzle)
-    } else if(e.target.readyState === 4) {
-      reject('An error has taken place')
-    }
-  })
+//   request.addEventListener('readystatechange', (e) => {
+//     if(e.target.readyState === 4 && e.target.status === 200) {
+//       const data = JSON.parse(e.target.responseText);
+//       resolve(data.puzzle)
+//     } else if(e.target.readyState === 4) {
+//       reject('An error has taken place')
+//     }
+//   })
 
-  request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
-  request.send();
-})
+//   request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+//   request.send();
+// })
 
 // // Making an HTTP request
 // const request = new XMLHttpRequest();
